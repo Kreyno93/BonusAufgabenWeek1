7182d0f9379f890d34e8f4867672dfb5dae879dc
Michael <michaelw.apple@icloud.com>
2023-04-20 15:07:33 +0200

    adding methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,50 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        printNumberFromArray(1);
+        printOneToHundreds();
+        printStringFromScanner();
+    }
 
+    public static void printNumberFromArray(int number) {
+        String[] numbers = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        System.out.println(numbers[number - 1]);
+    }
+
+    public static void printOneToHundreds() {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("#$");
+            } else if (i % 3 == 0) {
+                System.out.println("#");
+            } else if (i % 5 == 0) {
+                System.out.println("$");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void printStringFromScanner() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string or type exit to exit");
+        String[] inputArray = new String[0];
+
+        while(true){
+            System.out.println(Arrays.toString(inputArray));
+            String input = scanner.nextLine();
+            if(input.equals("exit")){
+                break;
+            }
+            String[] copyArray = Arrays.copyOf(inputArray, inputArray.length + 1);
+            copyArray[copyArray.length - 1] = input;
+            inputArray = copyArray;
+        }
     }
 }
